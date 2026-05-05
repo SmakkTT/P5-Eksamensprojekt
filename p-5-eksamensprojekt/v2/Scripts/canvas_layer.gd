cff69fb97b7145e43e428b1b6baa134831cc1d16
SmakkTT <85797241+SmakkTT@users.noreply.github.com>
2026-05-05 11:02:17 +0200

    1

--- p-5-eksamensprojekt/v2/Scripts/canvas_layer.gd
+++ p-5-eksamensprojekt/v2/Scripts/canvas_layer.gd
@@ -1,7 +1,9 @@
 extends CanvasLayer
 
-@onready var slider      = $Panel/VBoxContainer/HSlider
-@onready var angle_label = $Panel/VBoxContainer/AngleLabel
+@onready var slider       = $Panel/VBoxContainer/HSlider
+@onready var angle_label  = $Panel/VBoxContainer/AngleLabel
+@onready var tilt_slider  = $Panel/VBoxContainer/TiltSlider
+@onready var tilt_label   = $Panel/VBoxContainer/TiltLabel
 
 var current_box = null
 
@@ -9,13 +11,17 @@ func _ready():
 	# Skjul menu ved start
 	hide()
 	slider.value_changed.connect(_on_slider_value_changed)
+	tilt_slider.value_changed.connect(_on_tilt_slider_value_changed)
 
 func open_rotation_menu(box):
 	# Åbn menu og vis mus
 	current_box = box
-	var current_angle = current_box.get_parent().rotation_degrees.y
-	slider.value = current_angle
-	angle_label.text = str(int(current_angle)) + "°"
+	var current_y = current_box.get_parent().rotation_degrees.y
+	var current_x = current_box.get_parent().rotation_degrees.x
+	slider.value      = current_y
+	angle_label.text  = str(int(current_y)) + "°"
+	tilt_slider.value = current_x
+	tilt_label.text   = str(int(current_x)) + "°"
 	show()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
@@ -26,7 +32,7 @@ func close_rotation_menu():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
-	# Luk menu ved input
+	# Luk menu ved E eller ESC
 	if visible and (event.is_action_pressed("ui_cancel") or event.is_action_pressed("interact")):
 		get_viewport().set_input_as_handled()
 		var player = get_tree().get_first_node_in_group("Player")
@@ -34,8 +40,14 @@ func _input(event):
 			player.current_interactable = null
 		close_rotation_menu()
 
+## Opdaterer spejlets Y-rotation (venstre/højre)
 func _on_slider_value_changed(value):
-	# Opdater boksens vinkel
 	angle_label.text = str(int(value)) + "°"
 	if current_box:
 		current_box.set_y_rotation(value)
+
+## Opdaterer spejlets X-rotation (op/ned)
+func _on_tilt_slider_value_changed(value):
+	tilt_label.text = str(int(value)) + "°"
+	if current_box:
+		current_box.set_x_rotation(value)
--- p-5-eksamensprojekt/v2/Scripts/mirror.gd
+++ p-5-eksamensprojekt/v2/Scripts/mirror.gd
@@ -1,12 +1,17 @@
-# mirror.gd — Subclass of Interactable, handles mirror rotation
+# mirror.gd  Subclass af Interactable, håndterer spejlrotation (Y = venstre/højre, X = op/ned)
 class_name Mirror
 extends Interactable
 
 func interact() -> void:
 	get_tree().call_group("UI", "open_rotation_menu", self)
 
+## Drejer spejlet om Y-aksen (venstre/højre)
 func set_y_rotation(degrees: float) -> void:
 	get_parent().rotation_degrees.y = degrees
 
+## Vipper spejlet om X-aksen (op/ned)
+func set_x_rotation(degrees: float) -> void:
+	get_parent().rotation_degrees.x = degrees
+
 func get_interact_label_text() -> String:
-	return ""  # uses default "Press E to interact"
+	return ""  # bruger default "Press E to interact"
