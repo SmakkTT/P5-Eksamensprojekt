a70c9a2741981d8ed58136c6334774c42d8ec530
kleins <johansen.soren07@gmail.com>
2026-05-05 09:41:19 +0200

    remove junk

--- p-5-eksamensprojekt/v2/Scripts/static_body_3d.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody3D
-
-func interact():
-	get_tree().call_group("UI", "open_rotation_menu", self)
-
-func set_y_rotation(degrees: float):
-	# FIX: Roter MeshInstance3D (forælderen) — så mesh OG kollision drejer sammen
-	get_parent().rotation_degrees.y = degrees
